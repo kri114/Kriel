2a7e2a169af41a12ae233acfb657a65ad6978dcb
kri114 <kriklei.abdi@gmail.com>
2026-09-06 19:30:33 +0200

    Delete kriel-bronze-product-website directory

--- kriel-bronze-product-website/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getCatalog } from "@/db/catalog";
-import HomeClient from "@/components/HomeClient";
-
-export const dynamic = "force-dynamic";
-
-export default async function HomePage() {
-  const catalog = await getCatalog();
-  return <HomeClient catalog={catalog} />;
-}
